1dca72acccf8b7a2796ff6dd538d2caf4fc63129
Atef <selimatef07@gmail.com>
2025-01-26 00:31:21 +0200

    Handel Hadith and Quran Fragment

--- app/src/main/java/com/example/islamiapp/fragment/TasbeehFragment.kt
+++ app/src/main/java/com/example/islamiapp/fragment/TasbeehFragment.kt
@@ -1,5 +1,6 @@
 package com.example.islamiapp.fragment
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,11 +11,14 @@ import com.example.islamiapp.databinding.FragmentTasbeehBinding
 class TasbeehFragment : Fragment() {
     lateinit var binding: FragmentTasbeehBinding
     private var count = 0
+    private var rotationAngle = 0f
     private var zekrArray = arrayOf(
         "سُبْحَانَ اللَّهِ",
         "الْحَمْدُ لِلَّهِ",
         "لَا إِلَهَ إِلَّا اللَّهُ",
-        "اللَّهُ أَكْبَرُ")
+        "اللَّهُ أَكْبَرُ"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -26,22 +30,25 @@ class TasbeehFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentTasbeehBinding.inflate(inflater,container,false)
+        binding = FragmentTasbeehBinding.inflate(inflater, container, false)
 
         binding.zekrNameTv.text = zekrArray[0]
 
         binding.sebhaBodyImg.setOnClickListener {
             count++
-            if (count > 33){
+            if (count > 33) {
                 count = 1
                 val currentIndex = zekrArray.indexOf(binding.zekrNameTv.text.toString())
-                val nextIndex = (currentIndex+1) % zekrArray.size
+                val nextIndex = (currentIndex + 1) % zekrArray.size
                 binding.zekrNameTv.text = zekrArray[nextIndex].toString()
             }
             binding.zekrCountTv.text = count.toString()
+            rotationAngle += 30f
+            ObjectAnimator.ofFloat(binding.sebhaBodyImg, "rotation", rotationAngle).apply {
+                duration = 300
+                start()
 
-
-
+            }
         }
         return binding.root
     }
